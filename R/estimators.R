d04fdec9ae05f8a10e6ed2d9e4cba8dec29917f3
Antoine Rebecq-Roussel <haroine.git@rebecq.fr>
2016-02-04 13:49:58 +0100

    Change name of estimators functions (and add aliases)

--- R/estimators.R
+++ R/estimators.R
@@ -1,11 +1,40 @@
-# copyright (C) 2015 A.Rebecq
+# copyright (C) 2014-2016 A.Rebecq
 
-#' Computes the Horvitz-Thompson estimator for the total of a column
+#' Weighted estimator for total
+#' @description 
+#' Computes the weighted estimator for the total of a column. Alias for
+#' \code{\link{weightedTotal}}
 #' @param var column of variable of interest
 #' @param weights column of weights matching the variable of interest
 #' @return Estimated total
+#' @seealso \code{\link{weightedTotal}}
 #' @export
 HTtotal <- function(var, weights) {
+  return(weightedTotal(var, weights))
+}
+
+#' Weighted estimator for the mean
+#' @description 
+#' Computes the weighted estimator for the mean of a column. Alias for
+#' \code{\link{weightedMean}}
+#' @param var column of variable of interest
+#' @param weights column of weights matching the variable of interest
+#' @return Estimated mean
+#' @seealso \code{\link{weightedMean}}
+#' @export
+HTmean <- function(var, weights, popTot=NULL) {
+ return(weightedMean(var, weights, popTot)) 
+}
+
+#' Weighted estimator for total
+#' @description 
+#' Computes the weighted estimator for the total of a column
+#' @param var column of variable of interest
+#' @param weights column of weights matching the variable of interest
+#' @return Estimated total
+#' @seealso \code{\link{HTtotal}}
+#' @export
+weightedTotal <- function(var, weights) {
 
   if(!is.numeric(var)) {
     var <- as.numeric(var)
@@ -19,12 +48,15 @@ HTtotal <- function(var, weights) {
   return(var %*% weights)
 }
 
-#' Computes the Horvitz-Thompson estimator for the mean of a column
+#' Weighted estimator for the mean
+#' @description 
+#' Computes the weighted estimator for the mean of a column
 #' @param var column of variable of interest
 #' @param weights column of weights matching the variable of interest
 #' @return Estimated mean
+#' @seealso \code{\link{HTmean}}
 #' @export
-HTmean <- function(var, weights, popTot=NULL) {
+weightedMean <- function(var, weights, popTot=NULL) {
 
   # Population total defaults to sum of weights
   if(is.null(popTot)) {
