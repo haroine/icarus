169297dc061f571cbfd795c063ac58ea64b4f636
Antoine R <antoine@nc233.com>
2016-04-14 14:41:18 +0200

    Add example for regroupCalibrationModalities

--- R/calmarFunctions.R
+++ R/calmarFunctions.R
@@ -480,6 +480,29 @@ checkNumberMargins = function(data, marginMatrix) {
 #' @param calibrationVariable name of the calibration varaible for which regroupment has to be done
 #' @param vecModalities Initial modalities of the variable
 #' @param newModality Regrouped modalities of the variable
+#' 
+#' @examples
+#' ## Suppose we have a calibration matrix and a margin matrix containing information
+#' ## for two categorical variables "X1" (10 modalities) and "X2" (5 modalities)
+#' 
+#' table(matrixCal$X1)
+#' # 1  2  3  4  5  6  7  8  9 10 
+#' # 9  8  8  8 11 15 13  6 10 12 
+#' marginMatrix
+#' # [,1] [,2] [,3]  [,4]  [,5]  [,6]  [,7]  [,8]  [,9]  [,10] [,11] [,12]
+#' # [1,] "X1" "10" "0.1" "0.1" "0.1" "0.1" "0.1" "0.1" "0.1" "0.1" "0.1" "0.1"
+#' # [2,] "X2" "5"  "0.2" "0.2" "0.2" "0.2" "0.2" "0"   "0"   "0"   "0"   "0" 
+#' 
+#' regroupCalibrationModalities(matrixCal, marginMatrix, "X1", c(3,4,8), "0")
+#' 
+#' table(matrixCal$X1)
+#' # 0  1  2  5  6  7  9 10 
+#' # 22  9  8 11 15 13 10 12 
+#' marginMatrix
+#' # [,1] [,2] [,3]  [,4]  [,5]  [,6]  [,7]  [,8]  [,9]  [,10]
+#' # [1,] "X1" "8"  "0.3" "0.1" "0.1" "0.1" "0.1" "0.1" "0.1" "0.1"
+#' # [2,] "X2" "5"  "0.2" "0.2" "0.2" "0.2" "0.2" "0"   "0"   "0"  
+#' 
 #' @export
 regroupCalibrationModalities <- function(calibrationMatrix, marginMatrix, calibrationVariable, vecModalities, newModality) {
   
